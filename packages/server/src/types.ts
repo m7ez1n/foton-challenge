d8b06d12f3f0965f4068fef0aeef72632fee62fd
Mateus Mendes <mateuspm2015@hotmail.com>
2020-03-06 22:46:43 -0300

    feat(types): create types

---
+++ packages/server/src/types.ts
@@ -0,0 +1,17 @@
+import { IUser } from './modules/user/UserModel';
+
+import DataLoader from 'dataloader';
+import { Types } from 'mongoose';
+import { Context } from 'koa';
+
+export type DataLoaderKey = Types.ObjectId | string | undefined | null;
+
+export interface GraphQLDataloaders {
+  UserLoader: DataLoader<DataLoaderKey, IUser>;
+}
+
+export interface GraphQLContext {
+  dataloaders: GraphQLDataloaders;
+  appplatform: string;
+  koaContext: Context;
+}
