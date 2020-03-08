03b7d2b8a601a38570847c017d86a67aace1a0db
Mateus Mendes <mateuspm2015@hotmail.com>
2020-03-08 16:45:08 -0300

    feat(task): add task type

---
+++ packages/server/src/modules/tasks/TaskType.ts
@@ -0,0 +1,51 @@
+import { NodeInterface } from '../../interface/NodeInterface';
+
+import { connectionDefinitions } from '../../graphql/connection/CustomConnectionType';
+
+import { GraphQLContext } from '../../types';
+
+import Task from './TaskLoader';
+
+import { GraphQLID, GraphQLNonNull, GraphQLObjectType, GraphQLObjectTypeConfig, GraphQLString } from 'graphql';
+
+import { globalIdField } from 'graphql-relay';
+
+type ConfigType = GraphQLObjectTypeConfig<Task, GraphQLContext>;
+
+const TaskTypeConfig: ConfigType = {
+  name: 'Tasks',
+  description: 'Task data',
+  fields: {
+    id: globalIdField('Task'),
+    _id: {
+      type: GraphQLID,
+      resolve: task => task._id,
+    },
+    title: {
+      type: GraphQLString,
+      resolve: task => task.title,
+    },
+    description: {
+      type: GraphQLString,
+      resolve: task => task.description,
+    },
+    createdAt: {
+      type: GraphQLString,
+      resolve: ({ createdAt }) => (createdAt ? createdAt.toISOString() : null),
+    },
+    updatedAt: {
+      type: GraphQLString,
+      resolve: ({ createdAt }) => (createdAt ? createdAt.toISOString() : null),
+    },
+  },
+  interfaces: () => [NodeInterface],
+};
+
+const TaskType = new GraphQLObjectType(TaskTypeConfig);
+
+export const TaskConnection = connectionDefinitions({
+  name: 'Task',
+  nodeType: GraphQLNonNull(TaskType),
+});
+
+export default TaskType;
