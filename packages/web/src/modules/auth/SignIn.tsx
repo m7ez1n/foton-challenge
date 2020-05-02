411db3a74b5c0b497542c8ae2dac05746ead55ac
Mateus Mendes <mateuspm2015@hotmail.com>
2020-05-02 04:54:36 -0300

    fix(auth): formik forms and yup validation ref #4

--- packages/web/src/modules/auth/SignIn.tsx
+++ packages/web/src/modules/auth/SignIn.tsx
@@ -1,64 +1,67 @@
 import React from 'react';
+import * as Yup from 'yup';
 import { toast } from 'react-toastify';
+import { useFormik } from 'formik';
 import { useMutation, graphql } from 'relay-hooks';
+
 import history from '../../routes/history';
 
-import { SignInMutation, SignInMutationResponse } from './__generated__/SignInMutation.graphql';
+//import { SignInMutation, SignInMutationResponse } from './__generated__/SignInMutation.graphql';
 
 import AuthForm from './AuthForm';
 import { MailOutlined, LockOutlined } from '@ant-design/icons';
 
 const SignIn: React.FC = () => {
-  const [mutation, { loading }] = useMutation<SignInMutation>(
-    graphql`
-      mutation SignInMutation($input: UserLoginMutationInput!) {
-        UserLoginMutation(input: $input) {
-          token
-          error
-        }
-      }
-    `,
-    {
-      onCompleted: async ({ UserLoginMutation }: SignInMutationResponse) => {
-        if (UserLoginMutation!.error && !UserLoginMutation!.token) {
-          toast.error(`❌ Registration failed, ${UserLoginMutation!.error}`);
-        } else {
-          localStorage.setItem('token', UserLoginMutation!.token!);
-          history.push('/todo');
-        }
-      },
-      onError: () => {
-        toast.error('❌ Registration failed, network request failed');
-      },
+  // const [mutation, { loading }] = useMutation<SignInMutation>(
+  //   graphql`
+  //     mutation SignInMutation($input: UserLoginMutationInput!) {
+  //       UserLoginMutation(input: $input) {
+  //         token
+  //         error
+  //       }
+  //     }
+  //   `,
+  //   {
+  //     onCompleted: async ({ UserLoginMutation }: SignInMutationResponse) => {
+  //       if (UserLoginMutation!.error && !UserLoginMutation!.token) {
+  //         toast.error(`❌ Registration failed, ${UserLoginMutation!.error}`);
+  //       } else {
+  //         localStorage.setItem('token', UserLoginMutation!.token!);
+  //         history.push('/todo');
+  //       }
+  //     },
+  //     onError: () => {
+  //       toast.error('❌ Registration failed, network request failed');
+  //     },
+  //   },
+  // );
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
     },
-  );
+    validationSchema: Yup.object().shape({
+      email: Yup.string()
+        .email('E-mail needs to be a valid e-mail')
+        .required('E-mail is required'),
+      password: Yup.string().required('Password is required'),
+    }),
+    onSubmit: input => {
+      console.log(input);
+    },
+  });
 
   const fields = [
     {
       name: 'email',
       placeholder: 'Email',
-      rules: [
-        {
-          required: true,
-          message: 'Please input your email!',
-        },
-        {
-          type: 'email',
-          message: 'The input is not valid E-mail!',
-        },
-      ],
       icon: <MailOutlined />,
     },
     {
       name: 'password',
       placeholder: 'Password',
-      type: 'password',
-      rules: [
-        {
-          required: true,
-          message: 'Please input your email!',
-        },
-      ],
+      typePassword: true,
       icon: <LockOutlined />,
     },
   ];
@@ -70,7 +73,7 @@ const SignIn: React.FC = () => {
     },
   ];
 
-  return <AuthForm fields={fields} returnLink={link} buttonText="Login" />;
+  return <AuthForm formik={formik} fields={fields} returnLink={link} buttonText="Login" />;
 };
 
 export default SignIn;
--- packages/web/src/modules/auth/SignUp.tsx
+++ packages/web/src/modules/auth/SignUp.tsx
@@ -1,73 +1,53 @@
 import React from 'react';
+import * as Yup from 'yup';
 import { toast } from 'react-toastify';
 import { useMutation, graphql } from 'relay-hooks';
 import history from '../../routes/history';
 
 import AuthForm from './AuthForm';
 import { UserOutlined, LockOutlined, MailOutlined } from '@ant-design/icons';
+import { useFormik } from 'formik';
 
 const SignUp: React.FC = () => {
-  const [mutation, { loading }] = useMutation<SignUpMutation>(
-    graphql`
-      mutation RegisterUserEmail($input: UserRegisterMutationInput!) {
-        UserRegisterMutation(input: $input) {
-          token
-          error
-        }
-      }
-    `,
-    {
-      onCompleted: async ({ UserRegisterMutation }: SignUpMutationResponse) => {
-        if (UserRegisterMutation!.error && !UserRegisterMutation!.token) {
-          toast.error(`❌ Registration failed, ${UserRegisterMutation!.error}`);
-        } else {
-          localStorage.setItem('token', UserRegisterMutation!.token!);
-          history.push('/todo');
-        }
-      },
-      onError: () => {
-        toast.error('❌ Registration failed, network request failed');
-      },
-    },
-  );
+  // const [mutation, { loading }] = useMutation<SignUpMutation>(
+  //   graphql`
+  //     mutation RegisterUserEmail($input: UserRegisterMutationInput!) {
+  //       UserRegisterMutation(input: $input) {
+  //         token
+  //         error
+  //       }
+  //     }
+  //   `,
+  //   {
+  //     onCompleted: async ({ UserRegisterMutation }: SignUpMutationResponse) => {
+  //       if (UserRegisterMutation!.error && !UserRegisterMutation!.token) {
+  //         toast.error(`❌ Registration failed, ${UserRegisterMutation!.error}`);
+  //       } else {
+  //         localStorage.setItem('token', UserRegisterMutation!.token!);
+  //         history.push('/todo');
+  //       }
+  //     },
+  //     onError: () => {
+  //       toast.error('❌ Registration failed, network request failed');
+  //     },
+  //   },
+  // );
 
   const fields = [
     {
       name: 'username',
       placeholder: 'Username',
-      rules: [
-        {
-          required: true,
-          message: 'Please input your username!',
-        },
-      ],
       icon: <UserOutlined />,
     },
     {
       name: 'email',
       placeholder: 'Email',
-      rules: [
-        {
-          required: true,
-          message: 'Please input your email!',
-        },
-        {
-          type: 'email',
-          message: 'The input is not valid E-mail!',
-        },
-      ],
       icon: <MailOutlined />,
     },
     {
       name: 'password',
       placeholder: 'Password',
-      type: 'password',
-      rules: [
-        {
-          required: true,
-          message: 'Please input your email!',
-        },
-      ],
+      typePassword: true,
       icon: <LockOutlined />,
     },
   ];
@@ -79,7 +59,28 @@ const SignUp: React.FC = () => {
     },
   ];
 
-  return <AuthForm fields={fields} returnLink={link} buttonText="Signup" />;
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      email: '',
+      password: '',
+    },
+    validationSchema: Yup.object().shape({
+      username: Yup.string().required('Name is required'),
+      email: Yup.string()
+        .email('E-mail needs to be a valid e-mail')
+        .required('E-mail is required'),
+      password: Yup.string()
+        .required('Password is required')
+        .min(4, 'Password must be more than 4')
+        .max(16, "Password can't be more than 16"),
+    }),
+    onSubmit: input => {
+      console.log(input);
+    },
+  });
+
+  return <AuthForm formik={formik} fields={fields} returnLink={link} buttonText="Signup" />;
 };
 
 export default SignUp;
